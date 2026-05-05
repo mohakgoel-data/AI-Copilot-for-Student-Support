b23591191149e2d04fe5fb6c01e59d415cd1301f
mohakgoel-data <mohakgoel.data@gmail.com>
2026-05-05 12:27:24 +0530

    generation

---
+++ generation.py
@@ -0,0 +1,65 @@
+import os
+from google import genai
+from sqlalchemy.orm import Session
+from dotenv import load_dotenv
+from retrieval import get_query_embedding,search_relevant_chunks
+
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+def assemble_prompt(query_text, search_results):
+    docs_xml = "<docs>\n"
+    for res in search_results:
+
+        source_name = res['metadata'].get('source', 'Unknown Source')
+        
+        docs_xml += f'  <doc id="{res["chunk_id"]}" name="{source_name}">\n'
+        docs_xml += f'    {res["content"]}\n'
+        docs_xml += f'  </doc>\n'
+    docs_xml += "</docs>"
+
+    system_instruction = (
+        "You are an official AI Student Assistant for Scaler School of Technology. "
+        "Your task is to answer the user's question using ONLY the provided <docs>. "
+        "Rules:\n"
+        "1. If the answer is not in the <docs>, say: 'I am sorry, but I don't have information on that in my records.'\n"
+        "2. For every claim you make, you MUST cite the source name in square brackets, e.g., [hostel_manual.pdf].\n"
+        "3. Maintain a helpful, student-friendly tone."
+    )
+
+    final_prompt = f"""
+    {system_instruction}
+
+    {docs_xml}
+
+    USER QUESTION: {query_text}
+    """
+    return final_prompt
+
+def generate_response(session: Session, student_query: str):
+    query_vector = get_query_embedding(student_query)
+    raw_results = search_relevant_chunks(session, query_vector, top_k=6)
+    filtered_results = [r for r in raw_results if r['score'] > 0.47]
+
+    if not filtered_results:
+        return {
+            "answer": "I am sorry, but I don't have information on that in my records.",
+            "sources": []
+        }
+
+    prompt = assemble_prompt(student_query, filtered_results)
+
+    try:
+        response = client.models.generate_content(
+            model="gemini-3-flash-preview",
+            contents=prompt
+        )
+        
+        return {
+            "answer": response.text,
+            "sources": filtered_results 
+        }
+    except Exception as e:
+        print(f"Generation Error: {e}")
+        return {"answer": "An error occurred while generating a response.", "sources": []}
+    
