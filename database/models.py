c52f1500ed90518916e1f0805f579b33ef203c2f
mohakgoel-data <mohakgoel.data@gmail.com>
2026-05-09 00:58:33 +0530

    user table added and made a foreign key in chats table

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, JSON
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, JSON, Boolean
 from sqlalchemy.orm import declarative_base, relationship
 from pgvector.sqlalchemy import Vector
 from sqlalchemy.sql import func
@@ -33,13 +33,23 @@ class DocumentChunk(Base):
 
     document = relationship("Document")
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True, nullable=True)
+    password_hash = Column(String(255), nullable=True)
+    
+    is_admin = Column(Boolean, default=False)
+    
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
 class ChatMessage(Base):
     __tablename__ = "chat_messages"
 
     id = Column(Integer, primary_key=True)
 
-    user_id = Column(Integer, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
     role = Column(String(20))
 
